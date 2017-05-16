6548a3dc7bc63d37209817e0cfb7623211691724
lidiperma <lidiperma@gmail.com>
2017-05-16 12:23:44 +0200

    Es para mi repositorio local

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 
  
  
+/* global $scope */
+
 var app=angular.module("app",[]);
 
 function PruebaController($scope) {
   $scope.mensaje="Hola Mundo";
-  $scope.cambiarMensaje=function() {
-    $scope.mensaje="Adios mundo cruel :-)";
-  };
+  $scope.mensaje="Adios mundo cruel :-)";
   
-
+  $scope.cambiarMensaje2=function() {
+    $scope.mensaje="A tomar cafe";
+    $scope.mensaje2="Que sean dos";
+};
 }
+
+
 
   
   
